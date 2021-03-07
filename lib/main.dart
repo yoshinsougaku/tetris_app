35298405e94b02159816e3fcd544ac74f1694e6f
yoshinsougaku <>
2021-03-08 00:46:05 +0900

    Tミノ作成 t

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'TETRIS',
+      title: 'TETRISuu',
       home: Scaffold(
         body: Center(
           child: CustomPaint(
