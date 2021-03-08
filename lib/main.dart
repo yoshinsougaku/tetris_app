6b2713b458f706a632d8920a43e1b7fe1c4837f8
yoshinsougaku <>
2021-03-09 03:01:39 +0900

    ミノ作成

--- lib/main.dart
+++ lib/main.dart
@@ -9,13 +9,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'TETRISuu',
+      title: 'TETRIS',
       home: Scaffold(
-        body: Center(
-          child: CustomPaint(
-            size: Size(100,100),
-            painter: Mino(),
-          ),
+        body: CustomPaint(
+          size: Size(100,100),
+          painter: Mino(),
         )
       ),
     );
@@ -23,15 +21,42 @@ class MyApp extends StatelessWidget {
 }
 
 class Mino extends CustomPainter {
-  @override
+  double baseL = 30;
+  double baseP = 10;
+@override
   void paint(Canvas canvas, Size size) {
-    final paint = Paint();
+    Paint paint = Paint();
     paint.color = Colors.red;
-    canvas.drawRect(Rect.fromLTWH(0,0,90,30), paint);
-    canvas.drawRect(Rect.fromLTWH(30,-30,30,45), paint);
+    //T
+    canvas.drawRect(Rect.fromLTWH(220,450,baseL,baseL), paint);
+    canvas.drawRect(Rect.fromLTWH(220-baseL,450,baseL,baseL), paint);
+    canvas.drawRect(Rect.fromLTWH(220+baseL,450,baseL,baseL), paint);
+    canvas.drawRect(Rect.fromLTWH(220,450-baseL,baseL,baseL), paint);
+//sikaku
+    canvas.drawRect(Rect.fromLTWH(120,450,baseL,baseL), paint);
+    canvas.drawRect(Rect.fromLTWH(120-baseL,450,baseL,baseL), paint);
+    canvas.drawRect(Rect.fromLTWH(120,450-baseL,baseL,baseL), paint);
+    canvas.drawRect(Rect.fromLTWH(120-baseL,450-baseL,baseL,baseL), paint);
+//tyokusenn
+  canvas.drawRect(Rect.fromLTWH(220,250,baseL,baseL), paint);
+  canvas.drawRect(Rect.fromLTWH(220+baseL,250,baseL,baseL), paint);
+  canvas.drawRect(Rect.fromLTWH(220+baseL+baseL,250,baseL,baseL), paint);
+  canvas.drawRect(Rect.fromLTWH(220+baseL+baseL+baseL,250,baseL,baseL), paint);
+//naname
+  canvas.drawRect(Rect.fromLTWH(220,650,baseL,baseL), paint);
+  canvas.drawRect(Rect.fromLTWH(220+baseL,650,baseL,baseL), paint);
+  canvas.drawRect(Rect.fromLTWH(220+baseL,650+baseL,baseL,baseL), paint);
+  canvas.drawRect(Rect.fromLTWH(220+baseL+baseL,650+baseL,baseL,baseL), paint);
 
-  }
+//yoko3
+  canvas.drawRect(Rect.fromLTWH(320,550,baseL,baseL), paint);
+  canvas.drawRect(Rect.fromLTWH(320+baseL,550,baseL,baseL), paint);
+  canvas.drawRect(Rect.fromLTWH(320+baseL+baseL,550,baseL,baseL), paint);
+  canvas.drawRect(Rect.fromLTWH(320+baseL+baseL,550-baseL,baseL,baseL), paint);
+}
 
   @override
-  bool shouldRepaint(CustomPainter oldDelegate) => true;
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return true;
+  }
 }
