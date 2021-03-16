6f831352a143f9ddf59ae5e3b22d235101e6d310
yoshinsougaku <>
2021-03-16 11:46:05 +0900

    （未完）固定したやつの再描画

--- lib/mino.dart
+++ lib/mino.dart
@@ -1,25 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
-
-void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'TETRIS',
-      home: MainPage()
-    );
-  }
-}
 
 class Mino extends CustomPainter {
   double baseL = 30;
   double baseP = 10;
+  final double y;
+  final double x;
+  bool isPlaying;
+  double k;
 
-  void tPaintCell(Canvas canvas,Paint paint,double x,double y,){
+
+  Mino({@required this.x, @required this.y,this.isPlaying});
+
+  void tPaintCell(Canvas canvas,Paint paint,double x,double y){
     canvas.drawRect(Rect.fromLTWH(x*baseL,y*baseL,baseL,baseL), paint);
     canvas.drawRect(Rect.fromLTWH(x*baseL,y*baseL-baseL,baseL,baseL), paint);
     canvas.drawRect(Rect.fromLTWH(x*baseL-baseL,y*baseL,baseL,baseL), paint);
@@ -54,45 +46,31 @@ class Mino extends CustomPainter {
     canvas.drawRect(Rect.fromLTWH(x*baseL+baseL+baseL+baseL,y*baseL,baseL,baseL), paint);
   }
 
-@override
-  void paint(Canvas canvas, Size size,) {
+  void paintCell(Canvas canvas,Paint paint,k){
+    canvas.drawRect(Rect.fromLTWH(x, y*baseL, baseL, baseL), paint);
+
+  }
+
+  @override
+  void paint(Canvas canvas, Size size) {
     Paint paint = Paint();
     paint.color = Colors.red;
 
-    tPaintCell(canvas, paint, 2, 10);
-    oPaintCell(canvas, paint, 5, 3);
-    zPaintCell(canvas, paint, 8, 8);
-    iPaintCell(canvas, paint, 4, 12);
-    lPaintCell(canvas, paint, 6, 15);
-}
+    while(isPlaying) {
+      paintCell(canvas, paint,k);
+      if(isPlaying == false){
+        k = y;
+        paintCell(canvas, paint,k);
+      }
+    } // tPaintCell(canvas, paint, 2, 10);
+    // oPaintCell(canvas, paint, 5, 3);
+    // zPaintCell(canvas, paint, 8, 8);
+    // iPaintCell(canvas, paint, 4, 12);
+    // lPaintCell(canvas, paint, 6, 15);
+  }
 
   @override
   bool shouldRepaint(CustomPainter oldDelegate) {
     return true;
   }
-}
-
-class MainPage extends StatefulWidget {
-  MainPage({Key key, this.title}) : super(key: key);
-  final String title;
-
-  @override
-  _MainPageState createState() => _MainPageState();
-}
-
-class _MainPageState extends State<MainPage> {
-  int start = 10;
-  int current = 10;
-
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        body: CustomPaint(
-          size: Size(100,100),
-          painter: Mino(),
-        ),
-    );
-  }
-
 }
